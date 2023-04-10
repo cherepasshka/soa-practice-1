b169cb3c5e3b629ea2746b921a12dc8d7f55fe77
Darya Zhukova <cherepashka2080@gmail.com>
2023-04-10 04:37:28 +0300

    some of serialization methods

---
+++ serialization_module/json_serializer.py
@@ -0,0 +1,19 @@
+import json
+import time
+from serialization_module.serializer import Serializer
+
+class JsonSerializer(Serializer):
+    def get_serialization_method(self):
+        return 'JSON'
+
+    def serialize(self, msg: object):
+        start_time = time.time()
+        serialized = json.dumps(msg)
+        execution_time = time.time() - start_time
+        return serialized, execution_time
+    
+    def deserialize(self, msg: str):
+        start_time = time.time()
+        deserialized = json.loads(msg)
+        execution_time = time.time() - start_time
+        return deserialized, execution_time
---
+++ serialization_module/msgpack_serializer.py
@@ -0,0 +1,19 @@
+import msgpack
+import time
+from serialization_module.serializer import Serializer
+
+class MessagePackSerializer(Serializer):
+    def get_serialization_method(self):
+        return 'MessagePack'
+
+    def serialize(self, msg: object):
+        start_time = time.time()
+        serialized = msgpack.packb(msg)
+        execution_time = time.time() - start_time
+        return serialized, execution_time
+    
+    def deserialize(self, msg: str):
+        start_time = time.time()
+        deserialized = msgpack.unpackb(msg)
+        execution_time = time.time() - start_time
+        return deserialized, execution_time
