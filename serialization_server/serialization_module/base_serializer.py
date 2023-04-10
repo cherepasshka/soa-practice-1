0db46f754f3b07b08f9aeff7512f439914e3b4e7
Darya Zhukova <cherepashka2080@gmail.com>
2023-04-10 23:52:13 +0300

    changed time format

--- serialization_server/serialization_module/base_serializer.py
+++ serialization_server/serialization_module/base_serializer.py
@@ -18,6 +18,9 @@ class BaseSerializer(ABC):
     def get_message_size(self, msg: str):
         return sys.getsizeof(msg)
 
+    def convert_to_ms(self, seconds):
+        return round(seconds * 1000, 3)
+
     def get_info(self, msg: object):
         serialized, serialization_time = self.serialize(msg)
         deserealized, deserialization_time = self.deserialize(serialized)
@@ -25,5 +28,9 @@ class BaseSerializer(ABC):
         return f'Method: {self.get_serialization_method()}\n' + \
             f'Original size: {self.get_message_size(msg)} bytes\n' + \
             f'Serialized size: {self.get_message_size(serialized)} bytes\n' + \
-            f'Serialization time: {serialization_time}\n' + \
-            f'Deserialization time:{deserialization_time}\n'
+            f'Serialization time: {convert_to_ms(serialization_time)}ms\n' + \
+            f'Deserialization time:{convert_to_ms(deserialization_time)}ms\n'
+
+
+def convert_to_ms(seconds: float):
+    return round(seconds * 1000, 3)
